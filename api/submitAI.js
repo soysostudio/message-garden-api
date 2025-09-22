c772c70c05b257c6edc65f2640c2711e7fd6cc7e
soysostudio <soyso.studio@gmail.com>
2025-09-22 15:17:00 -0500

    Update submitAI.js
    Image size

--- api/submitAI.js
+++ api/submitAI.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
     const img = await openai.images.generate({
       model: "gpt-image-1",
       prompt: flowerPrompt,
-      size: "512x512",
+      size: "1024x1024",
       background: "transparent"
     });
 
