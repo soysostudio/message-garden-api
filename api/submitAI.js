21b0f1bdecd181de1c805e9d26c254f208241d39
soysostudio <soyso.studio@gmail.com>
2025-09-30 08:39:54 -0500

    Update submitAI.js

--- api/submitAI.js
+++ api/submitAI.js
@@ -79,7 +79,8 @@ export default async function handler(req, res) {
       model: "gpt-image-1",
       prompt: flowerPrompt,
       size: "1024x1024",
-      background: "transparent"
+      background: "transparent",
+      quality: "high"  
     });
     const pngBuffer = Buffer.from(img.data[0].b64_json, "base64");
 
